6578394ccfe7403eec0e4ff81e81602703ff537a
Neeraj Maurya <Neerj7668>
2021-02-04 16:34:40 +0530

    welcome page
    Signed-off-by: Neeraj Maurya <Neerj7668>

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
 import App from "./App";
 ReactDOM.render(<App />, document.getElementById("root"));
