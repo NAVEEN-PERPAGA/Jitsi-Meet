f32d6adff80178dc883c4d731e069c6c9ce69c76
Neeraj Maurya <Neerj7668>
2021-02-18 11:20:04 +0530

    Merge branch neeraj
    Signed-off-by: Neeraj Maurya <Neerj7668>

--- client/src/components/JitsiMeetComponent.js
+++ client/src/components/JitsiMeetComponent.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import queryString from "query-string";
 import ProgressComponent from "@material-ui/core/CircularProgress";
 const JitsiMeetComponent = () => {
   const [loading, setLoading] = useState(false);
